23dbcf380df369ad6563c4140b73a2f5e8dc5a04
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-04-06 12:16:40 -0300

    header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import "./Header.css";
+
+import { BiPencil } from "react-icons/bi";
+
+function Header() {
+  return (
+    <header>
+      <h1>Minhas tarefas</h1>
+      <span className="icon">
+        <BiPencil />
+      </span>
+    </header>
+  );
+}
+
+export default Header;
